a95de628947055e074322da5838dedccd2657479
Krzysztof Maicher <krzysztof.maicher@gmail.com>
2025-02-28 10:05:52 +0100

    Use bat to colorize fzf preview

--- lib/.config/nvim/after/plugin/fzf.lua
+++ lib/.config/nvim/after/plugin/fzf.lua
@@ -2,3 +2,6 @@ vim.keymap.set("n", "<C-g>", ":Rg<CR>")
 vim.keymap.set("n", "<C-f>", ":GFiles<CR>")
 vim.keymap.set("n", "<leader>/", ":BLines<CR>")
 vim.keymap.set("n", "<C-b>", ":Buffers<CR>")
+
+-- Make fzf use bat with syntax highlighting for all files
+vim.env.FZF_DEFAULT_OPTS = '--preview "bat --style=numbers --color=always --line-range :500 {}"'
